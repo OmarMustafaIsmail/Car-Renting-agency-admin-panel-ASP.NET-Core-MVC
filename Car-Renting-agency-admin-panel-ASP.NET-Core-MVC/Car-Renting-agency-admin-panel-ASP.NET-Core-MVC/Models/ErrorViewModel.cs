1755cdfce04264b5a2fff87647e95721b491cdd9
OmarMustafaIsmail <66802392+OmarMustafaIsmail@users.noreply.github.com>
2022-03-04 14:50:50 +0200

    init commit- setting up project

---
+++ Car-Renting-agency-admin-panel-ASP.NET-Core-MVC/Car-Renting-agency-admin-panel-ASP.NET-Core-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Car_Renting_agency_admin_panel_ASP.NET_Core_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
